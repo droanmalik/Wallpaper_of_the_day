40a5ad24a7cf3d5579a7e54fa3a4c61a07577f39
Droan Malik <droanmalik@gmail.com>
2015-02-13 16:13:29 +0530

    Separated fragnment class

---
+++ app/src/main/java/com/d_r_o_a_n/wallpaperoftheday/PlaceholderFragment.java
@@ -0,0 +1,24 @@
+package com.d_r_o_a_n.wallpaperoftheday;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Aaron on 13-Feb-15.
+ */
+class PlaceholderFragment extends Fragment {
+
+    public PlaceholderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
+
